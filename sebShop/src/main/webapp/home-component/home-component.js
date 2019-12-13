627ea0ec12d2ece1cf80a4a9c53eb0960238a7ce
david <david_junghans@web.de>
2019-12-13 14:35:43 +0100

    fixed it

--- sebShop/src/main/webapp/home-component/home-component.js
+++ sebShop/src/main/webapp/home-component/home-component.js
@@ -1,10 +1,13 @@
 
 
+console.log("used");
 
-$("#details-button").click(function(){
-    $("body").load("details-component/details-component.html");
-});
+    $("#details-button").click(function(){
+        console.log("detail");
+        $("body").load("details-component/details-component.html");
+    });
 
-$("#buy-button").click(function(){
-    $("body").load("production-list-component/product-list-component.html");
-});
+    $("#buy-button").click(function(){
+        console.log("buy");
+        $("body").load("production-list-component/product-list-component.html");
+    });
--- sebShop/src/main/webapp/login-component/login-component.js
+++ sebShop/src/main/webapp/login-component/login-component.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
+console.log("login");
     $("#login-button").click(function(){
+        console.log("tes");
         $("body").load("home-component/home-component.html");
     });
-});
