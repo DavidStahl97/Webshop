ebf9ba118e996fa226beb554fb67ce59f9e4a4b9
David Stahl <stahl.d@loh-services.de>
2019-12-29 16:56:33 +0100

    show received payment options

--- sebShop/src/main/webapp/payment-component/payment-component.js
+++ sebShop/src/main/webapp/payment-component/payment-component.js
@@ -1,15 +1,59 @@
+const payment_options = [
+    {
+        name: 'Paypal',
+        icon: 'fab fa-paypal'
+    },
+    {
+        name: 'Credit Card',
+        icon: 'far fa-credit-card'
+    },
+    {
+        name: 'Klarna',
+        icon: 'fab fa-cc-visa'
+    },
+    {
+        name: 'Bitcoin',
+        icon: 'fab fa-bitcoin'
+    },
+    {
+        name: 'Invoice',
+        icon: 'far fa-money-bill-alt'
+    }
+]
+
+function load_payments() {
+    $.ajax({
+        url: 'http://localhost:8080/paymentServlet',
+        data: {},
+        type: 'GET',
+        success: function (data) {
+            const received_payment_options = JSON.parse(data);
+
+            let i = 0;
+            for (const payment of received_payment_options) {
+                const payment_option = payment_options.find(x => x.name == payment);
+
+                const button_id = `payment-button-${ i }`;
+
+                $('#payment-option-row').append(`
+                    <div class="col">
+                        <button type="button" class="btn btn-link" id="${ button_id }">
+                            <i class="${ payment_option.icon } payment-button"></i>                            
+                        </button>
+                        <h3 class="text-center"><span class="badge badge-secondary">${ payment }</span></h3>
+                    </div>               
+                `);
+
+                $(`#${ button_id }`).click(function () {
+                    nextElement();
+                });
+
+                i++;
+            }
+        }
+    });
+}
+
 $(document).ready(function () {
-
-    $('#money-button').click(function () {
-        nextElement();
-    });
-
-    $('#visa-button').click(function () {
-        nextElement();
-    });
-
-    $('#paypal-button').click(function () {
-        nextElement();
-    });
-
+    load_payments();
 });
